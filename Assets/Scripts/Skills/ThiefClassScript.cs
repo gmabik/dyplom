a67dd2391573b9c3b1d209d7adc58f3f8373cec8
Vlad <vladik11012006@gmail.com>
2025-05-27 01:47:34 +0200

    thief + anim fix + scene save

---
+++ Assets/Scripts/Skills/ThiefClassScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefClassScript : ClassScript
+{
+    protected override void Skill()
+    {
+        gameObject.GetComponent<Attack>().RangedAttack();
+    }
+}
